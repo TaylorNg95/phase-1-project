87a9bfdfd791cd6e1aeb0c08e70a52e40b8e401e
Taylor Ng <taylormng95@gmail.com>
2024-05-23 12:05:29 -0400

    completes fetchData function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert('Welcome to my page!')
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+let drinksArray = []
+
+document.addEventListener('DOMContentLoaded', function(){
+    Array.from(alphabet).forEach(letter => fetchData(letter))
+})
+
+function fetchData(letter){
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
+        .then(response => response.json())
+        .then(data => {
+            if(data.drinks !== null){
+                drinksArray = drinksArray.concat(data.drinks)
+            }
+        })
+}
