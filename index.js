47368e14c61fd39d5338f305930c252f2d79e3f2
Taylor Ng <taylormng95@gmail.com>
2024-05-23 13:57:31 -0400

    completes majority of recursive fetchData function; needs to make adjustment to capture drinks starting with Z

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+let counter = 0
+
 let drinksArray = []
 
 document.addEventListener('DOMContentLoaded', function(){
-    Array.from(alphabet).forEach(letter => fetchData(letter))
+    fetchData('a')
 })
 
 function fetchData(letter){
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
         .then(response => response.json())
         .then(data => {
-            if(data.drinks !== null){
-                drinksArray = drinksArray.concat(data.drinks)
+            /* debugger */
+            if(counter < 25){
+                counter++
+                const newLetter = alphabet[counter]
+                if(data.drinks !== null){
+                    drinksArray = drinksArray.concat(data.drinks)
+                }
+                /* debugger */
+                fetchData(newLetter) // Needs to get the Z ones but not call the fetch
+            } else {
+                /* renderDrinks(drinksArray) */
             }
+           /*  if(data.drinks !== null){
+                drinksArray = drinksArray.concat(data.drinks)
+                data.drinks.forEach(drink => {
+                    renderDrink(drink)
+                })
+            } */
         })
 }
+
+
+/* function renderDrinks(drinksArray){
+    console.log(drinksArray)
+} */
+
+
