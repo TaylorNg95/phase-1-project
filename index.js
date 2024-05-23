efac84c6d42934a2059b070530e628d2177b24fb
Taylor Ng <taylormng95@gmail.com>
2024-05-23 15:03:33 -0400

    completes recursive fetchData function

--- index.js
+++ index.js
@@ -12,26 +12,25 @@ function fetchData(letter){
         .then(response => response.json())
         .then(data => {
             /* debugger */
-            if(counter < 25){
-                counter++
-                const newLetter = alphabet[counter]
+            if(counter < 26){
                 if(data.drinks !== null){
                     drinksArray = drinksArray.concat(data.drinks)
                 }
                 /* debugger */
-                fetchData(newLetter) // Needs to get the Z ones but not call the fetch
+                const newLetter = alphabet[counter + 1]
+                if(newLetter !== undefined){
+                    fetchData(newLetter)
+                }
+                counter++
             } else {
                 /* renderDrinks(drinksArray) */
             }
-           /*  if(data.drinks !== null){
-                drinksArray = drinksArray.concat(data.drinks)
-                data.drinks.forEach(drink => {
-                    renderDrink(drink)
-                })
-            } */
         })
 }
-
+/* Line 21  becomes relevant at the last array - we want to increment NewLetter so that we can make
+a recursive function call, but when we are in the 'Z' drinks, we want to add them to the drinksArray
+(i.e. implement the first if) but NOT call the fetch again (i.e. implement the second if), so we only
+increment the counter which then results in the else statement. */
 
 /* function renderDrinks(drinksArray){
     console.log(drinksArray)
