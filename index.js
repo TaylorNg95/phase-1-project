fee2eaa4db317e75baf7bf04ba0cb2f10968b6f1
Taylor Ng <taylormng95@gmail.com>
2024-05-24 09:19:47 -0400

    removes commented test code that is no longer relevany for render functions

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ function fetchAllDrinks(letter){
     )
 }
 
+/* Line 21  becomes relevant at the last array - we want to increment NewLetter so that we can make
+a recursive function call, but when we are in the 'Z' drinks, we want to add them to the drinksArray
+(i.e. implement the first if) but NOT call the fetch again (i.e. implement the second if), so we only
+increment the counter which then results in the else statement. REVIEW THIS LATER */
+
 function renderAllDrinks(drinksArray){
     drinksArray.forEach(drink => {
         renderOneDrink(drink)
@@ -58,35 +63,5 @@ function calcIngredientsArray(drinksArray){
 
     })
 }
-
-document.querySelector('#img-vodka').addEventListener('click', () => alert('Vodka!'))
-
-/* function renderAllDrinks(drinks){
-    Array.from(drinks).forEach(drink => {
-        console.log(drink)
-    });
-} */
-
-/* function renderAllDrinks(drinksArray){
-    drinksArray.forEach(drink => {
-        renderOneDrink(drink)
-    }) 
-}
-
-function renderOneDrink(drink){
-    const img = document.createElement('img')
-    img.src = drink.strDrink
-    const h1 = document.createElement('h1')
-} */
-/* Line 21  becomes relevant at the last array - we want to increment NewLetter so that we can make
-a recursive function call, but when we are in the 'Z' drinks, we want to add them to the drinksArray
-(i.e. implement the first if) but NOT call the fetch again (i.e. implement the second if), so we only
-increment the counter which then results in the else statement. */
-
-/* function renderAllDrinks(){
-    drinksArray.forEach(drink => {
-        console.log(drink.idDrink)}
-    )
-} */
 
 
