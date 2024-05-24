4f67d1b5fab3555bd3f8b409df927aca80442fb0
Taylor Ng <taylormng95@gmail.com>
2024-05-24 09:58:14 -0400

    adds keepIngredients function to strip out ingredients from each drink

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let counter = 0
 let drinksArray = []
 let ingredientsArray = []
 
-document.addEventListener('DOMContentLoaded', function(){
+/* document.addEventListener('DOMContentLoaded', function(){
     fetchAllDrinks('a')
 })
 
@@ -28,10 +28,10 @@ function fetchAllDrinks(letter){
     )
 }
 
-/* Line 21  becomes relevant at the last array - we want to increment NewLetter so that we can make
+Line 21  becomes relevant at the last array - we want to increment NewLetter so that we can make
 a recursive function call, but when we are in the 'Z' drinks, we want to add them to the drinksArray
 (i.e. implement the first if) but NOT call the fetch again (i.e. implement the second if), so we only
-increment the counter which then results in the else statement. REVIEW THIS LATER */
+increment the counter which then results in the else statement. REVIEW THIS LATER
 
 function renderAllDrinks(drinksArray){
     drinksArray.forEach(drink => {
@@ -54,14 +54,48 @@ function renderOneDrink(drink){
     const drinksContainer = document.querySelector('#data')
     drinksContainer.appendChild(drinkCard)
     console.log('finished')
+} */
 
-    calcIngredientsArray(drinksArray)    
+document.addEventListener('DOMContentLoaded', function(){
+    fetchAllDrinks('a')
+})
+
+function fetchAllDrinks(letter){
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
+        .then(response => response.json())
+        .then(data => {
+            if(data.drinks !== null){
+                ingredientsArray = ingredientsArray.concat(data.drinks)
+            }
+            if(counter < 25){
+                const newLetter = alphabet[counter + 1]
+                if(newLetter !== undefined){
+                    fetchAllDrinks(newLetter)
+                }
+                counter++
+            } else {
+                ingredientsArray.forEach(drink => {
+                    keepIngredients(drink)
+                })
+            }
+        }
+    )
 }
 
-function calcIngredientsArray(drinksArray){
-    drinksArray.forEach(drink => {
+function keepIngredients(drink){
+    const {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15} = drink
+    const ingredients = {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15}
+    console.log(ingredients)
+    /* let counter = 1
+    const maxIngredients = 15
+    let ingredientQuery = `strIngredient${counter}`
 
-    })
+    while(counter <= maxIngredients){
+        console.log(drink[ingredientQuery])
+        counter++
+    } */
+
+    /* console.log(drink[`${maxIngredients}`]) */
 }
 
 
