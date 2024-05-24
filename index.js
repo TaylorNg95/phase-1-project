236613addc045a6856dea9df44df6bd3e11c9169
Taylor Ng <taylormng95@gmail.com>
2024-05-24 15:48:29 -0400

    tries function to retrieve drinks; need to fix this because right now it is returning the wrong drinks based on userIngredient input

--- index.js
+++ index.js
@@ -8,58 +8,6 @@ let userIngredients = []
 const btnContainer = document.querySelector('#btn-container')
 const drinksContainer = document.querySelector('#drinks-container')
 
-/* document.addEventListener('DOMContentLoaded', function(){
-    fetchAllDrinks('a')
-})
-
-function fetchAllDrinks(letter){
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`)
-        .then(response => response.json())
-        .then(data => {
-            if(data.drinks !== null){
-                drinksArray = drinksArray.concat(data.drinks)
-            }
-            if(counter < 25){
-                const newLetter = alphabet[counter + 1]
-                if(newLetter !== undefined){
-                    fetchAllDrinks(newLetter)
-                }
-                counter++
-            } else {
-                renderAllDrinks(drinksArray)
-            }
-        }
-    )
-}
-
-Line 21  becomes relevant at the last array - we want to increment NewLetter so that we can make
-a recursive function call, but when we are in the 'Z' drinks, we want to add them to the drinksArray
-(i.e. implement the first if) but NOT call the fetch again (i.e. implement the second if), so we only
-increment the counter which then results in the else statement. REVIEW THIS LATER
-
-function renderAllDrinks(drinksArray){
-    drinksArray.forEach(drink => {
-        renderOneDrink(drink)
-        console.log(drink)
-    })
-}
-
-function renderOneDrink(drink){
-    const img = document.createElement('img')
-    const h3 = document.createElement('h3')
-
-    h3.textContent = drink.strDrink
-    img.src = drink.strDrinkThumb
-
-    const drinkCard = document.createElement('div')
-    drinkCard.className = 'drink-card'
-    drinkCard.append(img, h3)
-
-    const drinksContainer = document.querySelector('#data')
-    drinksContainer.appendChild(drinkCard)
-    console.log('finished')
-} */
-
 document.addEventListener('DOMContentLoaded', function(){
     fetchAllDrinks('a')
 })
@@ -94,6 +42,10 @@ function populateDrinksArray(data){
         renderIngredient(ingredient)
     })
 }
+
+/* The data.drinks !== null has to come first so that the Z drinks are loaded into the array. But then when
+counter < 25 condition is tested, it does not pass, which means that there are no more fetches made. This
+avoids a fetch call to undefined. REVIEW THIS  */
 
 function keepIngredients(drink){
     const {strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15} = drink
@@ -149,8 +101,29 @@ function addEvents(btn){
     })
 }
 
-document.querySelector('#make-me-drinks').addEventListener('click', function(event){
-    event.preventDefault()
+document.querySelector('#make-me-drinks').addEventListener('click', function(){
     btnContainer.classList.add('hidden')
     drinksContainer.classList.remove('hidden')
+
+    drinksArray.forEach(drink => {
+        let counter = 1
+        let drinkKey = `strIngredient${counter}`
+        let ingArray = []
+        let status = true
+        while(counter <= 15){
+            if(drink[drinkKey] !== null){
+                ingArray.push(drink[drinkKey])
+            }
+            counter++
+        }
+        ingArray.forEach(ing => {
+            const search = userIngredients.find(userIng => userIng === ing)
+            if(search === undefined){status = false}
+        })
+        if(status === true){renderDrink(drink)} 
+    })
 })
+
+function renderDrink(drink){
+    console.log(drink.strDrink)
+}
